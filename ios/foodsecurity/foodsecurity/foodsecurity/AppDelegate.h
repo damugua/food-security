ca173e5b16c841cf0763995f8ab38f048e351159
ARonDellon <wangweiyi@wangweiyideMacBook-Pro.local>
2015-03-20 14:29:25 +0800

    create IOS

---
+++ ios/foodsecurity/foodsecurity/foodsecurity/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  foodsecurity
+//
+//  Created by wangweiyi on 15/3/20.
+//  Copyright (c) 2015年 com.zsgj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
