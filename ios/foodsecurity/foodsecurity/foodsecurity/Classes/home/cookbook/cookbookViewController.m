606beec1fc3466489cba314c428fb9ee58efae1c
fuyang <fuyang@jifencaideiMac.local>
2015-03-26 16:50:47 +0800

    cookBook类写了五个button

--- ios/foodsecurity/foodsecurity/foodsecurity/Classes/home/cookbook/cookbookViewController.m
+++ ios/foodsecurity/foodsecurity/foodsecurity/Classes/home/cookbook/cookbookViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "cookbookViewController.h"
+#define FRAME ([[UIScreen mainScreen] bounds])
 
 @interface cookbookViewController ()
 
@@ -16,7 +17,47 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self createNavigationBar];
+    [self createWeekButtons];
     // Do any additional setup after loading the view from its nib.
+}
+
+-(void)createNavigationBar
+{
+    self.navigationItem.title = @"食谱";
+    UIButton *leftButton = [UIButton buttonWithType:UIButtonTypeInfoLight];
+    [leftButton setImage:[UIImage imageNamed:@""] forState:UIControlStateNormal];
+    [leftButton addTarget:self action:@selector(leftButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
+    UIBarButtonItem *leftButtonItem = [[UIBarButtonItem alloc]initWithCustomView:leftButton];
+    self.navigationItem.leftBarButtonItem = leftButtonItem;
+}
+
+-(void)createWeekButtons
+{
+    NSLog(@"kuan %f",FRAME.size.width);
+    NSArray *weekArray=[NSArray arrayWithObjects:@"周一",@"周二",@"周三",@"周四",@"周五", nil];
+    for (int i=0; i<weekArray.count; i++) {
+        UIButton *weekButton=[UIButton buttonWithType:UIButtonTypeCustom];
+        weekButton.frame=CGRectMake((FRAME.size.width/5)*i, 64, FRAME.size.width/5, 40);
+        [weekButton setTitle:[weekArray objectAtIndex:i] forState:UIControlStateNormal];
+        [weekButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        weekButton.tag=i+1;
+        weekButton.layer.borderColor=[UIColor lightGrayColor].CGColor;
+        weekButton.layer.borderWidth=0.5;
+        [weekButton addTarget:self action:@selector(weekButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
+        [self.view addSubview:weekButton];
+    }
+    
+}
+
+-(void)leftButtonClicked:(UIButton *)button
+{
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+-(void)weekButtonClicked:(UIButton *)button
+{
+    NSLog(@"周 %d",button.tag);
 }
 
 - (void)didReceiveMemoryWarning {
