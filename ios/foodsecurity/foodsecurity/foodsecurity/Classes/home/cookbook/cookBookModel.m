5326c3bf264e239fcdb28fac95c7d8e7812b401c
fuyang <fuyang@jifencaideiMac.local>
2015-03-30 12:28:30 +0800

    updata  食谱暂时不做

---
+++ ios/foodsecurity/foodsecurity/foodsecurity/Classes/home/cookbook/cookBookModel.m
@@ -0,0 +1,19 @@
+//
+//  cookBookModel.m
+//  foodsecurity
+//
+//  Created by fuyang on 15/3/30.
+//  Copyright (c) 2015年 com.zsgj. All rights reserved.
+//
+
+#import "cookBookModel.h"
+
+@implementation cookBookModel
+
+-(void)setParameter:(NSDictionary *)cookBookData
+{
+    NSLog(@"cookBookData %@",cookBookData);
+}
+
+
+@end
